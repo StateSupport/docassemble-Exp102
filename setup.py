6d28b2c9a7c4ca745aa0d4f2d1e4a87c3fc07c57
Anonymous User <21244564+StateSupport@users.noreply.github.com>
2023-02-15 15:51:45 -0500

    Fixed code to display court address for 1 court and dropdown for multiple courts, and removed trial_court_district from interview and added to Attachments w/if statements

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(name='docassemble.Exp102',
       url='https://courtformsonline.org',
       packages=find_packages(),
       namespace_packages=['docassemble'],
-      install_requires=['docassemble.AssemblyLine>=2.19.0'],
+      install_requires=['docassemble.AssemblyLine>=2.21.0'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/Exp102/', package='docassemble.Exp102'),
      )
